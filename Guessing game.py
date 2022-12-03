1232c16d43acefdf93706eab2dd8b71c08b13be6
Bobby835 <117555260+Bobby835@users.noreply.github.com>
2022-12-02 21:53:36 -0500

    Guessing Game
    Guessing Game

---
+++ Guessing game.py
@@ -0,0 +1,12 @@
+print("Make a guessing game for which city is better!")
+print("===")
+print()
+city = input("Name your favorite city! ")
+if city == "rome":
+  print("Rome is awesome! ")
+elif city == "new york":
+  print("New York Rocks!! ")
+elif city == "miami":
+  print("miami is cool! ")
+else:
+  print("I don't travel much either so that's ok!")
